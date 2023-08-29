c0fb9b52d62114d8a01eba4284a27136042d2aaa
Muneera Altanam <51013336+MuneeraYousaf@users.noreply.github.com>
2023-08-29 23:25:33 +0300

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Muneera Y on 13/02/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
